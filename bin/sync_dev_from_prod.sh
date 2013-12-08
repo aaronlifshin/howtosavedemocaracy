ae8e622845981436e04ef7c5a0c65de941f11668
aaronlifshin <aaronlifshin@gmail.com>
2013-12-08 14:10:36 -0800

    COmment levels limited to 4. Update to sync script. Update to indices

--- bin/sync_dev_from_prod.sh
+++ bin/sync_dev_from_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 today=$(date "+%Y%m%d")
 appcfg.py download_data --url=http://howtosavedemocracy.appspot.com/_ah/remote_api --filename=/tmp/h2sd-prod-$today
-appcfg.py upload_data --url=http://localhost:8081/_ah/remote_api --filename=/tmp/h2sd-prod-$today
+appcfg.py upload_data --url=http://localhost:8082/_ah/remote_api --filename=/tmp/h2sd-prod-$today
